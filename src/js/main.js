db5976bf1142bce00997d3f15ea759b7e0e513bd
lvaldenebro <lauravaldeal@gmail.com>
2022-11-13 14:45:40 +0100

    Adds the code for the filter in the click event

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,41 @@
 'use strict';
 
-/*innput: 
-elinput busca personajes por su título*/
+// HTML SELECTORS
 
-// HTML selectors
 const input = document.querySelector('.js-input');
 const button = document.querySelector('.js-button');
 const section2 = document.querySelector('.js-section2');
 
-//Global variables
+//GLOBAL VARIABLES
+
 let charactersList = [];
 let characterFeatures = [];
 
-/*
-    //asumo q la api devuelve un obj
-    //cuando se recargue la página, recjo la info del obj y la pinto con DOM
-*/
+//FUNCTIONS
 
-    
+function matchCharacter(list) {
+    const allCharacters = document.querySelectorAll('.bb-character');
+
+    for (let character of allCharacters) {
+        //.childNodes access to the article children, with [i], access to the h4 position, with .text access to the value of h4
+        if (!character.childNodes[1].textContent.toLowerCase().includes(input.value.toLowerCase())) {
+            character.classList.add("hidden");
+        }
+    }
+}
+
+function handleClick(e) {
+    e.preventDefault();
+    matchCharacter(charactersList);
+}
+
 function receivedCharactersList(list) {
     for (const character of list) {
         let img = character.img;
         let name = character.name;
         let status = character.status;
         characterFeatures.push([img, name, status]); //pushes a list of 3 elements so characterFeatures would be a list of lists one of each character. [] build the list in the moment
-        console.log(characterFeatures);
+        //console.log(characterFeatures);
     }
 }
 
@@ -34,9 +45,11 @@ function renderHTMLCards() {
         //Creates elements in DOM
         const newArticle = document.createElement("article");
         const newImage = document.createElement("img");
-        const newName = document.createElement("name");
+        const newName = document.createElement("h4");
         const newParagraph = document.createElement("p");
-    
+        
+        //Adds classes to newArticle
+        newArticle.classList.add("bb-character") //maybe another with the name of the character
         
         //Adds content to the new elements in DOM
         newImage.src = character[0];//because img is an empty element and does not have content, we need to add the content to source attribute
@@ -52,14 +65,20 @@ function renderHTMLCards() {
         newArticle.appendChild(newParagraph);
 
         section2.appendChild(newArticle);
-        console.log(section2);
     }
 }
 
-    /*     //pido al API y pinto todo lo que me devulve */
+//CALL FUNCTIONS
+returnServerInfo();
+
+//EVENTS
+
+button.addEventListener('click', handleClick);
+
+//WHEN THE WEBPAGE LOADS
 
 function returnServerInfo() {
-    fetch('https://breakingbadapi.com/api/characters')
+    fetch('https://breakingbadapi.com/api/characters?limit=10&offset=0') //TODO: remove limit, this is set to help the code devlopment
         .then(function (response) { //promise
         return response.json(); //acts like stringify
         })
@@ -69,8 +88,3 @@ function returnServerInfo() {
         renderHTMLCards();
     });
 }
-
-returnServerInfo();
-
-
-    //pido a la api en base al texto del botón y pinto lo que me devuleve 
