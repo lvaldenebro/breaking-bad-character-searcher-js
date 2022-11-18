0ad6a3e259731ccb33833c93f67fb8ed61879cae
lvaldenebro <lauravaldeal@gmail.com>
2022-11-18 08:52:41 +0100

    Technical interview

--- src/js/3_filter.js
+++ src/js/3_filter.js
@@ -1,9 +1,32 @@
 /* eslint-disable indent */
 'use strict';
+const length = document.querySelector('.js-number-characters');
+let filteredCharacters = [];
+
+function comparisson(length) {
+    const array = [2, 5, 9];
+    for (const n of array) {
+        if (n < length) {
+            console.log(`${n} es menor que ${length}`);
+        } else if (n > length) {
+            console.log(`${n} es mayor que ${length}`);
+        } else {
+            console.log('Es igual');
+        }
+    }
+}
+
+function handleComparisson(e) {
+    // e.preventDefault();
+    comparisson(filteredCharacters.length);
+
+}
 
 function matchCharacter() {
     const userInput = input.value.toLowerCase();
-    const filteredCharacters = charactersList.filter((eachChar) => eachChar.name.toLowerCase().includes(userInput));
+    filteredCharacters = charactersList.filter((eachChar) => eachChar.status.toLowerCase().includes(userInput));
+    const filteredCharLenght = filteredCharacters.length;
+    length.innerHTML = filteredCharLenght;
 
     renderHTMLCards(filteredCharacters);
 }
@@ -14,3 +37,4 @@ function handleButtonClick(e) {
 }
 
 button.addEventListener('click', handleButtonClick);
+length.addEventListener('click', handleComparisson);
